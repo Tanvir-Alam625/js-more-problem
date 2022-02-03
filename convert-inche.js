4e41aaebd5d0e45dd07809c975c040c46547216e
tanveer <mdtanviralamht731@gmail.com>
2022-02-03 16:53:32 +0600

    inche and meter conveter file added

---
+++ convert-inche.js
@@ -0,0 +1,10 @@
+// input
+const  feets = 1.5;
+// function 
+function feetToInche(feet) {
+    let inche = feet * 12;
+    return inche;
+}
+// call function 
+const output = feetToInche(feets);
+console.log(output);
---
+++ convert-meter.js
@@ -0,0 +1,11 @@
+//input
+const centimeters = 123;
+// function 
+function centimeterToMeter(centimeter) {
+    let meter = centimeter/100;
+    return meter;
+    
+}
+// calling function 
+const output = centimeterToMeter(centimeters);
+console.log(output);
