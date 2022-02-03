2ff763d6af620157392b1f720880e0944c7e15a7
tanveer <mdtanviralamht731@gmail.com>
2022-02-03 17:15:06 +0600

    book-pages file added

---
+++ book-pages.js
@@ -0,0 +1,23 @@
+// Page requirment on three books 
+// input 
+const Mathemetics = 1;
+const Bangla = 0;
+const English = 1;
+
+// function 
+function pageRequirments( Mathemetics, Bangla, English) {
+    const mathPerBook = 100;
+    const banglaPerBook = 200;
+    const englishPerBook = 300;
+    if(typeof Mathemetics != 'number' || typeof Bangla != 'number' || typeof English != 'number'){
+        return " please input the number";
+    }
+    const mathBook = mathPerBook * Mathemetics;
+    const banglaBook = banglaPerBook * Bangla;
+    const englishBook = englishPerBook * English;
+    const totalPages = mathBook + banglaBook + englishBook;
+    return totalPages; 
+}
+// calling function 
+const output = pageRequirments(Mathemetics,Bangla,English);
+console.log(output);
